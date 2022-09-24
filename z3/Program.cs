4e9f7f23e28861d877b330908134f175ad997e8c
Tatiana <tt_tanya_88@mail.ru>
2022-09-24 12:42:28 +0300

    Zadanie3

---
+++ z3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите четверть: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+
+    if (i == 1)
+    {
+        Console.Write ("x > 0, y >0");
+    }
+    if (i == 2)
+    {
+        Console.Write ("x < 0, y >0");
+    }
+    if (i == 3)
+    {
+        Console.Write ("x < 0, y < 0");
+    }
+    if (i == 4)
+    {
+        Console.Write ("x > 0, y < 0");
+    }
+ else{
+    Console.WriteLine ("Неть! Можно только 1, 2, 3, 4");
+ }
+
