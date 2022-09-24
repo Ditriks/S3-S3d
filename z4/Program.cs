c19d3cd590151befa93bf52cd378a1f5633ec980
Tatiana <tt_tanya_88@mail.ru>
2022-09-24 12:42:42 +0300

    Zadanie4

---
+++ z4/Program.cs
@@ -0,0 +1,18 @@
+﻿
+    Console.Write("x1=");
+    x1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("x2=");
+
+    x2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("y1=");
+
+    y1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("y2=");
+
+    y2 = Convert.ToDouble(Console.ReadLine());
+           
+            
+        i = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+                Console.WriteLine("Расстояние между точками: " + i);
+                
