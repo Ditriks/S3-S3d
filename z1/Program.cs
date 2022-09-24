388750a0cb470bb389019eb75c0a3cc29d7bf135
Tatiana <tt_tanya_88@mail.ru>
2022-09-24 12:42:03 +0300

    Zadanie1

---
+++ z1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Console.Clear();
+
+//int num = 3;
+
+//int[] numbers = new int[5];
+//int[] numbers = new int[5] {1, 2, 3, 4, 5};
+//int[] numbers = new int[] {1, 2, 3, 4, 5};
+//int[] numbers = new [] {1, 2, 3, 4, 5};
+int[] numbers = {1, 2, -3, 4, 5};
+string[] name = {"Ann", "Bob", "John", "Kris", "Ama"};
+
+Console.WriteLine($"{numbers [2]} !!!");
+//numbers[3] = numbers[3] + 10;
+Console.Write($"{Math.Pow(numbers[2], 3)} !!!");
+
+//for (int i = 0; i < 5; i++)
+    //Console.WriteLine($"{i} элемент - это {name[i]} !!!");
+
